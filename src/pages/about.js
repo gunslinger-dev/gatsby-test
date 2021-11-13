27809047f0973a90ad3340dc706c50a59ba0842e
Mike Maberry <m.maberry@yottaa.com>
2021-11-13 08:10:12 -0500

    addeded components

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const About = () => {
+    return(
+        <Layout pageTitle="About">
+
+            <p>I made this site!</p>
+
+        </Layout>
+    )
+}
+
+export default About
